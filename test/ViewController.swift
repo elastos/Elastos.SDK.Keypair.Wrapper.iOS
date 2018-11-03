34f5f0f8d7ffae5a4bc27c2b8ac250a7c97e0745
mengxk <xiaokun.meng@qcast.cn>
2018-11-03 23:38:29 +0800

    add test

---
+++ test/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by mengxk on 2018/11/3.
+//  Copyright © 2018 Elastos. All rights reserved.
+//
+
+import UIKit
+
+import ElastosWalletLib
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //ElastosWallet.
+  }
+
+
+}
+
