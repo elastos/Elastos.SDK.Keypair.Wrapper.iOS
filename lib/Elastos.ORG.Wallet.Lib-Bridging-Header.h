cf25041b9915487d462da4c08c7761f53f72610c
mengxk <xiaokun.meng@qcast.cn>
2018-12-15 18:42:18 +0800

    add build.sh, can build arm64 and x86_64.

---
+++ lib/Elastos.ORG.Wallet.Lib-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  breadwallet-Bridging-Header.h
+//  Elastos.ORG.Wallet.Lib.iOS
+//
+//  Created by mengxk on 2018/11/3.
+//  Copyright © 2018 Elastos. All rights reserved.
+//
+
+#import "Elastos.ORG.Wallet.Lib.C/Elastos.Wallet.Utility.h"
+
---
+++ scripts/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash - 
+
+set -o errexit
+set -o nounset
+
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
+PROJECT_DIR=$(dirname "${SCRIPT_DIR}");
+BUILD_NAME="$PROJECT_DIR/build";
+
+PROJECT_NAME="Elastos.ORG.Wallet.Lib";
+PROJECT_BUILDTYPE="Release";
+
+TARGET_PATH="$BUILD_NAME/${PROJECT_NAME}.framework";
+
+cd "$PROJECT_DIR";
+xcodebuild -target "lib" -configuration "${PROJECT_BUILDTYPE}" -arch arm64  -sdk "iphoneos"; # defines_module=yes
+xcodebuild -target "lib" -configuration "${PROJECT_BUILDTYPE}" -arch x86_64 -sdk "iphonesimulator"; # defines_module=yes
+
+rm -rf "$TARGET_PATH";
+cp -r "$BUILD_NAME/${PROJECT_BUILDTYPE}-iphoneos/${PROJECT_NAME}.framework" "$TARGET_PATH";
+
+
+rm "$TARGET_PATH/$PROJECT_NAME";
+lipo -create -output "$TARGET_PATH/$PROJECT_NAME" \
+	"$BUILD_NAME/${PROJECT_BUILDTYPE}-iphoneos/${PROJECT_NAME}.framework/$PROJECT_NAME" \
+	"$BUILD_NAME/${PROJECT_BUILDTYPE}-iphonesimulator/${PROJECT_NAME}.framework/$PROJECT_NAME" \
+
+echo "Done!!!";
+
