76b535814b3504fec7be55de138ecbcf1ef522d8
mengxk <xiaokun.meng@qcast.cn>
2018-12-26 17:18:57 +0800

    add submodule depends/Elastos.ORG.Wallet.Lib.C

---
+++ scripts/build-depends.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+CURRENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
+PROJECT_DIR=$(dirname "$CURRENT_DIR")
+DEPENDS_DIR="$PROJECT_DIR/depends";
+
+git -C "$PROJECT_DIR" submodule init;
+git -C "$PROJECT_DIR" submodule update;
+
+export PACKAGE_PLATFORM=iOS;
+"$DEPENDS_DIR/Elastos.ORG.Wallet.Lib.C/scripts/package-mobile.sh";
+
+rm -rf "$PROJECT_DIR/frameworks";
+mkdir "$PROJECT_DIR/frameworks";
+
+cp -rv "$DEPENDS_DIR/Elastos.ORG.Wallet.Lib.C/build/package/Elastos.ORG.Wallet.Lib.C.framework" "$PROJECT_DIR/frameworks";
