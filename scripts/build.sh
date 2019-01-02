5e6697b190847c68107406d659fa81cebc931b10
mengxk <xiaokun.meng@qcast.cn>
2019-01-02 11:48:54 +0800

    add tag to scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,8 +9,10 @@ BUILD_NAME="$PROJECT_DIR/build";
 
 PROJECT_NAME="ElastosOrgWalletLib";
 PROJECT_BUILDTYPE="Release";
+PROJECT_REVISION="$(git rev-list --count HEAD)";
+PROJECT_VERSION="v0.1.$PROJECT_REVISION";
 
-TARGET_PATH="$BUILD_NAME/${PROJECT_NAME}.framework";
+TARGET_PATH="$BUILD_NAME/${PROJECT_NAME}-${PROJECT_VERSION}.framework";
 
 cd "$PROJECT_DIR";
 xcodebuild -target "lib" -configuration "${PROJECT_BUILDTYPE}" -arch arm64  -sdk "iphoneos"; # defines_module=yes
@@ -25,6 +27,7 @@ rm -rf "$TARGET_PATH/_CodeSignature";
 lipo -create -output "$TARGET_PATH/$PROJECT_NAME" \
 	"$BUILD_NAME/${PROJECT_BUILDTYPE}-iphoneos/${PROJECT_NAME}.framework/${PROJECT_NAME}" \
 	"$BUILD_NAME/${PROJECT_BUILDTYPE}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME}" \
+git tag --force ${PROJECT_VERSION}
 
 echo "Done!!!";
 
