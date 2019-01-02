550be46e3fbf6956ca473b11237dd957af822450
mengxk <xiaokun.meng@qcast.cn>
2019-01-02 14:02:16 +0800

    add version by build

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,8 +15,12 @@ PROJECT_VERSION="v0.1.$PROJECT_REVISION";
 TARGET_PATH="$BUILD_NAME/${PROJECT_NAME}-${PROJECT_VERSION}.framework";
 
 cd "$PROJECT_DIR";
-xcodebuild -target "lib" -configuration "${PROJECT_BUILDTYPE}" -arch arm64  -sdk "iphoneos"; # defines_module=yes
-xcodebuild -target "lib" -configuration "${PROJECT_BUILDTYPE}" -arch x86_64 -sdk "iphonesimulator"; # defines_module=yes
+export CURRENT_PROJECT_VERSION=${PROJECT_REVISION};
+export CURRENT_PROJECT_VERSIONNAME=${PROJECT_VERSION/v/};
+xcodebuild -target "lib" -configuration "${PROJECT_BUILDTYPE}" -arch arm64  -sdk "iphoneos" \
+	CURRENT_PROJECT_VERSION=${PROJECT_REVISION} CURRENT_PROJECT_VERSIONNAME=${PROJECT_VERSION/v/};
+xcodebuild -target "lib" -configuration "${PROJECT_BUILDTYPE}" -arch x86_64 -sdk "iphonesimulator" \
+	CURRENT_PROJECT_VERSION=${PROJECT_REVISION} CURRENT_PROJECT_VERSIONNAME=${PROJECT_VERSION/v/};
 
 rm -rf "$TARGET_PATH" && mkdir -p "$TARGET_PATH";
 cp -r "$BUILD_NAME/${PROJECT_BUILDTYPE}-iphonesimulator/${PROJECT_NAME}.framework/"* "$TARGET_PATH/";
@@ -26,7 +30,7 @@ rm "$TARGET_PATH/${PROJECT_NAME}";
 rm -rf "$TARGET_PATH/_CodeSignature";
 lipo -create -output "$TARGET_PATH/$PROJECT_NAME" \
 	"$BUILD_NAME/${PROJECT_BUILDTYPE}-iphoneos/${PROJECT_NAME}.framework/${PROJECT_NAME}" \
-	"$BUILD_NAME/${PROJECT_BUILDTYPE}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME}" \
+	"$BUILD_NAME/${PROJECT_BUILDTYPE}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME}";
 git tag --force ${PROJECT_VERSION}
 
 echo "Done!!!";
